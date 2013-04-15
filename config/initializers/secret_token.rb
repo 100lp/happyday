6cbf61a12fbd8a9698402271a809664fc4dc5a64
100lp <spb.gurme@gmail.com>
2013-04-15 21:56:04 +0400

    Our first happy project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happyday::Application.config.secret_token = '58e339034e496154b6cc4bb44c5c08485549c8b252796cdc6f0a73659d2510bb64da53550ab1af1b5046d6416d05fc18b99e2e3159aeb766d93a9b02b7410add'
